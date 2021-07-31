029b9945623c5571d01e6c7421e8b48d34d661f8
cotsao <codytsao@gmail.com>
2021-07-31 16:00:01 -0700

    completed assignment

---
+++ models/marsMissions.js
@@ -0,0 +1,39 @@
+
+const marsMissions = [
+    {
+      name: "Curiosity",
+      launchDate: "26 Nov 2011",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://render.fineartamerica.com/images/rendered/square-product/small/images/artworkimages/mediumlarge/2/the-nasa-mars-curiosity-rover-one-badass-ride-nasa.jpg"
+    },
+    {
+      name: "Opportunity",
+      launchDate: "8 Jul 2003",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://mars.nasa.gov/layout/general/images/mer.png"
+    },
+    {
+      name: "Spirit",
+      launchDate: "10 Jun 2003",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://i.pinimg.com/originals/00/78/8f/00788f035dc5fb1d9d5220b268a54a14.png"
+    },
+    {
+      name: "Sojourner",
+      launchDate: "4 Dec 1996",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://render.fineartamerica.com/images/rendered/square-dynamic/small/images-medium-large-5/mars-sojourner-detlev-van-ravenswaay.jpg"
+    },
+    {
+      name: "Rosetta",
+      launchDate: "2 Mar 2004",
+      operator: "ESA",
+      missionType: "Gravity Assist",
+      img: "https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2007/02/image_of_mars_seen_by_osiris/10287257-2-eng-GB/Image_of_Mars_seen_by_OSIRIS_card_medium.jpg"
+    }
+  ];
+  module.exports = marsMissions;
--- server.js
+++ server.js
@@ -1,25 +1,56 @@
 // DEPENDENCIES
 const express = require('express');
+const missions = require('./models/marsMissions.js') 
+var rowdy = require('rowdy-logger')
+
+
 const app = express();
+var rowdyResults = rowdy.begin(app)
 
 // run `npm install` to install dependencies in package.json
 
+
+/* Middleware */
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:false})) // adds form data, if present to req.body
+app.use(express.static(__dirname + '/public'));
 // * Your mission is to complete the app
 // * The app will need routes for index and show
 // * The app will need views for index and show
+
+
+app.get('/', (req,res) =>{
+    res.send("homepage")
+
+})
 
 // INDEX Route
 // send data to 'missions/index.ejs' view
 // the view should display just the names of each mission
 // display the mission names as <li> in a <ul> with the class name "missions"
+app.get('/missions', (req,res) =>{
+  res.render('index.ejs',{
+    allMissions: missions
+  })
+})
 
 // SHOW Route
 // send data to 'missions/show.ejs' view
 // the view should display all the data for a single mission
+app.get('/missions/:id', (req,res) =>{
+  res.render('show.ejs',{
+    oneMission: missions[req.params.id]
+  })  
+})
+
+
 
 // * Bonus/Hungry for More: User should be able to click on a mission’s name on the index page, and be taken to that mission’s show page to view the data
+// change to a tag
 // * Bonus/Hungry for More: add images to the data and have them display (google how)
+// find some 
 // * Bonus/Hungry for More: add static css to style the pages (google how)
+
 
 // NOTES:
 // ejs has not been installed
@@ -32,48 +63,14 @@ const port = 3000;
 // DATA - move this to a file called marsMissions.js inside of a models folder
 // remember to export (module.exports)
 // remember to require it in the server
-const marsMissions = [
-  {
-    name: "Curiosity",
-    launchDate: "26 Nov 2011",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Opportunity",
-    launchDate: "8 Jul 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Spirit",
-    launchDate: "10 Jun 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Sojourner",
-    launchDate: "4 Dec 1996",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Rosetta",
-    launchDate: "2 Mar 2004",
-    operator: "ESA",
-    missionType: "Gravity Assist",
-    img: ""
-  }
-];
+
 
 
 // LISTENER
 app.listen(port, function() {
-  console.log('Missions to Mars running on port: ', port);
+  console.log(`Example app listening at http://localhost:${port}`);
+  rowdyResults.print()
 })
+
 
 module.exports = app;
