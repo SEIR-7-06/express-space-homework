85104a70eaa36697e57bac33760601fd5da30218
gonzzoh <gmeza0118@gmail.com>
2021-07-31 19:38:54 -0400

    did something very minor

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require('express');
 const app = express();
+const missions = require('./models/marsMissions.js')
 
 // run `npm install` to install dependencies in package.json
 
@@ -13,9 +14,21 @@ const app = express();
 // the view should display just the names of each mission
 // display the mission names as <li> in a <ul> with the class name "missions"
 
+
+
 // SHOW Route
 // send data to 'missions/show.ejs' view
 // the view should display all the data for a single mission
+
+app.get('/missions/',(req, res) => {
+  res.send(missions);
+})
+
+app.get('/missions/:missionsIndex', (req,res) => {
+  res.render('show.ejs', {
+    oneMission: missions[req.params.missionsIndex]
+  })
+})
 
 // * Bonus/Hungry for More: User should be able to click on a mission’s name on the index page, and be taken to that mission’s show page to view the data
 // * Bonus/Hungry for More: add images to the data and have them display (google how)
@@ -32,43 +45,6 @@ const port = 3000;
 // DATA - move this to a file called marsMissions.js inside of a models folder
 // remember to export (module.exports)
 // remember to require it in the server
-const marsMissions = [
-  {
-    name: "Curiosity",
-    launchDate: "26 Nov 2011",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Opportunity",
-    launchDate: "8 Jul 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Spirit",
-    launchDate: "10 Jun 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Sojourner",
-    launchDate: "4 Dec 1996",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Rosetta",
-    launchDate: "2 Mar 2004",
-    operator: "ESA",
-    missionType: "Gravity Assist",
-    img: ""
-  }
-];
 
 
 // LISTENER
