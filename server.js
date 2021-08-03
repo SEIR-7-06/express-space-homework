861c166092dd5d291b24d953eb3aea30c2048253
off2mxrs <off2mxrs@gmail.com>
2021-08-03 16:58:20 -0700

    Setup complete. mars object connected

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // DEPENDENCIES
 const express = require('express');
 const app = express();
+const marsMissions = require('./models/marsMissions')
 
-// run `npm install` to install dependencies in package.json
+
 
 // * Your mission is to complete the app
 // * The app will need routes for index and show
@@ -21,54 +22,10 @@ const app = express();
 // * Bonus/Hungry for More: add images to the data and have them display (google how)
 // * Bonus/Hungry for More: add static css to style the pages (google how)
 
-// NOTES:
-// ejs has not been installed
-// views folder has not been created
-// views/missions folder has not been created
+
 
 // PORT
 const port = 3000;
-
-// DATA - move this to a file called marsMissions.js inside of a models folder
-// remember to export (module.exports)
-// remember to require it in the server
-const marsMissions = [
-  {
-    name: "Curiosity",
-    launchDate: "26 Nov 2011",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Opportunity",
-    launchDate: "8 Jul 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Spirit",
-    launchDate: "10 Jun 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Sojourner",
-    launchDate: "4 Dec 1996",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Rosetta",
-    launchDate: "2 Mar 2004",
-    operator: "ESA",
-    missionType: "Gravity Assist",
-    img: ""
-  }
-];
 
 
 // LISTENER
